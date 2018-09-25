b16402aed13389bdb1b411a8a9fe66675d809945
sopasdigital <rob.hilado@gmail.com>
2018-09-25 11:44:16 +0100

    Added country code to results to make it clear as to which city is displayed

--- src/containers/ForecastList/index.js
+++ src/containers/ForecastList/index.js
@@ -1,34 +1,42 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import './ForecastList.css';
+import "./ForecastList.css";
 
 class ForecastList extends Component {
   renderForecast(cityData) {
     const name = cityData.city.name;
-    const weatherData = cityData.list.map( data => {
+    const country = cityData.city.country;
+    const weatherData = cityData.list.map(data => {
       const i = cityData.list.indexOf(data) + 8;
 
-      if ( i%8 === 0 ) {
-        const formattedDate = data.dt_txt.replace(/-/g,",");
+      if (i % 8 === 0) {
+        const formattedDate = data.dt_txt.replace(/-/g, ",");
         const d = new Date(formattedDate);
         const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
         const day = days[d.getDay()];
 
-        return(
+        return (
           <span key={data.dt_txt} className="Entry">
             <span className="Entry-Label">{day}</span>
-            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt={data.weather[0].main} />
+            <img
+              src={`http://openweathermap.org/img/w/${
+                data.weather[0].icon
+              }.png`}
+              alt={data.weather[0].main}
+            />
           </span>
         );
       }
 
       return null;
     });
-    
+
     return (
       <li key={name} className="WeatherList-Item">
-        <span className="Entry-Title">{name}</span>
+        <span className="Entry-Title">
+          {name} ({country})
+        </span>
         {weatherData}
       </li>
     );
@@ -38,9 +46,7 @@ class ForecastList extends Component {
     return (
       <div className="u-width60">
         <ul className="WeatherList">
-          {
-            this.props.weather.map(this.renderForecast)
-          }
+          {this.props.weather.map(this.renderForecast)}
         </ul>
       </div>
     );
